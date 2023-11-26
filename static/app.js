bfede79d224338e079f4e4e01067d615cf987e6e
엘풍 <eruhoon@gmail.com>
2023-11-26 22:09:21 +0900

    update app.js

--- static/app.js
+++ static/app.js
@@ -1,5 +1,9 @@
+import { dev } from '$app/environment';
+
 if ('serviceWorker' in navigator) {
 	addEventListener('load', function () {
-		navigator.serviceWorker.register('./service-worker.js');
+		navigator.serviceWorker.register('./service-worker.js', {
+            type: dev ? 'module' : 'classic'
+        });
 	});
 }
