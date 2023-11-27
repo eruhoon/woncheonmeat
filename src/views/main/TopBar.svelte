7c455c93f70ee3ef9b6335b7942fb78d3f5efef5
엘풍 <eruhoon@gmail.com>
2023-11-28 00:47:52 +0900

    setup page

---
+++ src/views/main/TopBar.svelte
@@ -0,0 +1,19 @@
+<!-- svelte-ignore a11y-missing-attribute -->
+<div class="top-bar">
+	<img src="./favicon.png" />
+	<div class="title">패밀리쥐</div>
+</div>
+
+<style lang="scss">
+	.top-bar {
+		display: flex;
+		width: 100%;
+		height: 100%;
+
+		.title {
+			display: flex;
+			align-items: center;
+			margin: 0 5px;
+		}
+	}
+</style>
