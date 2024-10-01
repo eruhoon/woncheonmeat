a12669b364adc79ff674c224f3b88b683c17e5c6
엘풍 <eruhoon@gmail.com>
2024-10-01 14:27:13 +0900

    init

---
+++ src/views/component/BpmComponent.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    export let bpm: number = 180;
+</script>
+
+<div>
+    <div class="frame tweaker">
+        <button on:click={() => (bpm -= 10)}>➖</button>
+        <input type="number" value={bpm} />
+        <button on:click={() => (bpm += 10)}>➕</button>
+    </div>
+    <div class="frame preset">
+        <button on:click={() => (bpm = 120)}>120</button>
+        <button on:click={() => (bpm = 180)}>180</button>
+    </div>
+</div>
+
+<style lang="scss">
+    div {
+        width: 100%;
+        height: 100%;
+    }
+
+    div.frame {
+        margin: 10px 0;
+    }
+
+    div.tweaker {
+        $button-size: 30px;
+
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+
+        input {
+            text-align: center;
+            border: solid 0.5px;
+            border-radius: 100px;
+            background: transparent;
+            font-size: 50px;
+            width: 200px;
+            height: 200px;
+        }
+        button {
+            width: 30px;
+            height: 30px;
+            border-radius: 15px;
+            border: none;
+            background: transparent;
+            font-size: 30px;
+            text-align: center;
+            margin: 0;
+            padding: 0;
+        }
+    }
+</style>
--- src/views/main/MainPage.svelte
+++ src/views/main/MainPage.svelte
@@ -1,25 +1,42 @@
 <script lang="ts">
-    import Card from '../card/Card.svelte';
-    import TopBar from './TopBar.svelte';
+    import { onMount } from 'svelte';
+    import BpmComponent from '../component/BpmComponent.svelte';
 
-    async function fetchInven() {
-        const url = 'https://ikkanezumi-meow-fairy.koyeb.app/maple-inven';
-        const response = await fetch(url);
-        return await response.json();
+    let bpm: number = 180;
+    let beat: number = 4;
+
+    let counter = 0;
+    let timer: number | null = null;
+
+    let player1: HTMLAudioElement;
+    let player2: HTMLAudioElement;
+
+    function play() {
+        const oneMinute = 60 * 1000;
+        const unit = oneMinute / bpm;
+        let now = new Date().getTime();
+        timer = setInterval(() => {
+            player2.play();
+        }, unit);
     }
 </script>
 
-<div class="nav">
-    <TopBar />
-</div>
+<div class="nav"></div>
 <div class="content">
-    {#await fetchInven() then articles}
-        {#each articles as article}
-            <Card image={article.icon ?? './favicon.png'} title={article.title} link={article.link}
-            ></Card>
-        {/each}
-    {/await}
-    <Card image="./favicon.png"></Card>
+    <h3>BPM</h3>
+    <BpmComponent {bpm} />
+</div>
+
+<div class="content">
+    <h3>Beat</h3>
+    <button on:click={() => (beat = 3)}>3</button>
+    <button on:click={() => (beat = 4)}>4</button>
+</div>
+
+<div class="content">
+    <button on:click={() => play()}>▶️</button>
+    <audio src="%sveltekit.assets%/sound/metronome1.mp3" bind:this={player1}></audio>
+    <audio src="%sveltekit.assets%/sound/metronome2.mp3" bind:this={player2}></audio>
 </div>
 
 <style lang="scss">
@@ -37,6 +54,8 @@
     }
 
     .content {
+        align-items: center;
+        text-align: center;
         border: solid 1px #5d6582;
         border-radius: 5px;
         margin: 5px;
@@ -44,5 +63,13 @@
         width: calc(100% - 20px);
         height: calc(100% - $nav-height - 20px);
         background: #ffffff;
+    }
+
+    input {
+        font-size: 30px;
+    }
+
+    button {
+        font-size: 30px;
     }
 </style>
